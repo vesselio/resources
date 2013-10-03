31ecd12e9f33c145966604fc43c572dfe82bfd88
andykmccoy <andy@vessel.io>
2013-10-03 10:32:03 -0700

    tabs are not sticky across page loads

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    var tabs = 
+    {
+        init : function() {
+            this.applyPreviousActiveTab();
+            this.addListener('a[data-toggle="tab"]');
+        },
+        tabClickCallback : function(event) {
+            var $target = $(event.target),
+                href = $target.attr('href');
+
+            this.setActiveTab(href);
+        },
+        addListener : function(selector) {
+            $(selector).on('click', function(event){
+                tabs.tabClickCallback(event);
+            });
+        },
+        getActiveTab : function() {
+            return localStorage.getItem('activetab');
+        },
+        setActiveTab : function(href) {
+            localStorage.setItem('activetab', href);
+        },
+        applyPreviousActiveTab : function() {
+            var href = this.getActiveTab();
+            if(href){
+                $('a[href="' + href + '"]').tab('show');
+            }
+        }
+    };
+    tabs.init();
 });
